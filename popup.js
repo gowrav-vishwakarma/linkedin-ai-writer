3d127590139dd565d19cbedd84f070290e5d8e28
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2023-10-13 12:40:28 +0530

    Second way working... need btter prompts now

--- popup.js
+++ popup.js
@@ -1,3 +1,122 @@
+const DEFAULT_PROMPTS = [
+  {
+    label: "$ _",
+    position: "new_post",
+    text: "${editingBoxText}",
+    replaceText: true,
+  },{
+    label: "$ _",
+    position: "comment",
+    text: "${editingBoxText}",
+    replaceText: true,
+  },
+    {
+    label: "Professionalize",
+    position: "new_post",
+    text: "Please convert this text to professional language for my linkedin post: ${editingBoxText}",
+      replaceText: true,
+  },,
+    {
+    label: "Professionalize comment",
+    position: "comment",
+    text: "Please convert this text to professional language for my linkedin comment for a post where this is post content \n\n\n\n '''${postContent}''' \n\n\n And this is what I want to say \n\n\n ${editingBoxText}",
+        replaceText: true,
+  },
+  {
+    label: "ToHindi",
+    position: "comment",
+    text: "can you translate the following content to Hindi:\n\n ${postContent}",
+        replaceText: false,
+  },
+];
+
+chrome.storage.local.get("chrome_openai_prompts", (data) => {
+  if (!data.chrome_openai_prompts || data.chrome_openai_prompts.length === 0) {
+    chrome.storage.local.set({ chrome_openai_prompts: DEFAULT_PROMPTS }, () => {
+      // After setting the default prompts, load them into the UI
+      DEFAULT_PROMPTS.forEach(createPromptUI);
+    });
+  } else {
+    // If prompts already exist, load them into the UI
+    data.chrome_openai_prompts.forEach(createPromptUI);
+  }
+});
+
+const promptsContainer = document.getElementById("prompts-container");
+
+const promptsTbody = document.getElementById("prompts-tbody");
+
+function createPromptUI(prompt) {
+  const tr = document.createElement("tr");
+
+  const tdPosition = document.createElement("td");
+  const positionSelect = document.createElement("select");
+  positionSelect.className = "form-control";
+  ["new_post", "comment"].forEach(pos => {
+    const option = document.createElement("option");
+    option.value = pos;
+    option.textContent = pos;
+    if (pos === prompt.position) option.selected = true;
+    positionSelect.appendChild(option);
+  });
+  tdPosition.appendChild(positionSelect);
+  tr.appendChild(tdPosition);
+
+  const tdLabel = document.createElement("td");
+  const labelInput = document.createElement("input");
+  labelInput.className = "form-control";
+  labelInput.placeholder = "Label";
+  labelInput.value = prompt.label;
+  tdLabel.appendChild(labelInput);
+  tr.appendChild(tdLabel);
+
+  const tdText = document.createElement("td");
+  const textInput = document.createElement("textarea");
+  textInput.className = "form-control";
+  textInput.placeholder = "Prompt Text";
+  textInput.value = prompt.text;
+  tdText.appendChild(textInput);
+  tr.appendChild(tdText);
+
+  const tdReplaceText = document.createElement("td");
+  const replaceCheckbox = document.createElement("input");
+  replaceCheckbox.type = "checkbox";
+  replaceCheckbox.checked = prompt.replaceText || false;
+  tdReplaceText.appendChild(replaceCheckbox);
+  tr.appendChild(tdReplaceText);
+
+  const tdActions = document.createElement("td");
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "btn btn-danger btn-sm";
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    tr.remove();
+  });
+  tdActions.appendChild(deleteButton);
+  tr.appendChild(tdActions);
+
+  promptsTbody.appendChild(tr);
+}
+
+
+document.getElementById("add-prompt").addEventListener("click", () => {
+  createPromptUI({ label: "", position: "new_post", text: "" });
+});
+
+
+document.getElementById("save-prompts").addEventListener("click", () => {
+  const prompts = Array.from(promptsTbody.children).map(tr => {
+    return {
+      label: tr.querySelector("input").value,
+      position: tr.querySelector("select").value,
+      text: tr.querySelector("textarea").value,
+        replaceText: tr.querySelector("input[type='checkbox']").checked,
+    };
+  });
+  chrome.storage.local.set({ chrome_openai_prompts: prompts });
+});
+
+
 const apiKeyInput = document.getElementById("api-key");
 console.log("apiKeyInput", apiKeyInput); // log the apiKeyInput to verify it exists
 
@@ -11,7 +130,7 @@ chrome.storage.local.get("chrome_openai_apiKey", (data) => {
 
 // Handle form submission
 const form = document.querySelector("form");
-const submitBtn = document.querySelector("input[type='submit']");
+const submitBtn = document.querySelector("button[type='submit']");
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
